3106eedfb5f911d1332f5ad01ba0643c245bc0af
aakarsheeee <88969304+aakarsheeee@users.noreply.github.com>
2021-11-24 16:30:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,7 @@ xhr.onload=function(){
                   <h2 class="accordion-header" id="heading${index}">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}"
                       aria-expanded="true" aria-controls="collapseOne">
-                      <img src="${element["image"]}" class="img-thumbnail mx-2" style="height:100px;" alt="image">
-                      ${element["title"]}
+                      ${element["title"]}<img src="${element["image"]}" class="img-thumbnail mx-2" style="height:100px;" alt="image">
                     </button>
                   </h2>
                   <div id="collapse${index}" class="accordion-collapse collapse show" aria-labelledby="heading${index}"
